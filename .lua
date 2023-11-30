93bf9e262b6aceed0e20f44fdc959084f87c55ff
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-12-01 08:03:38 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,5 @@
+local UI = {}
+
 local Domain = {
 Domain = Instance.new("ScreenGui"),
 Main = Instance.new("Frame"),
@@ -14,20 +16,23 @@ Other = Instance.new("Frame"),
 Shadow_11 = Instance.new("ImageLabel")
 }
 
+local shop = false
+local StopShop = false
+
 Domain.Domain.Name = "Domain"
 Domain.Domain.Parent = game:GetService("CoreGui")
 Domain.Domain.ResetOnSpawn = false
 
-Domain.Main.Name = "Main"
+--[[Domain.Main.Name = "Main"
 Domain.Main.Parent = Domain.Domain
 Domain.Main.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
 Domain.Main.BorderSizePixel = 0
 Domain.Main.Position = UDim2.new(0.966215432, 0, 0.262220383, 0)
 Domain.Main.Size = UDim2.new(0.0426387787, 0, 0.474966884, 0)
-Domain.Main.ZIndex = 5
+Domain.Main.ZIndex = 5]]
 
 Domain.UICorner.CornerRadius = UDim.new(0, 12)
-Domain.UICorner.Parent = Domain.Main
+Domain.UICorner.Parent = Domain.Domain
 
 Domain.Other.Name = "Other"
 Domain.Other.Parent = Domain.Domain
@@ -38,7 +43,7 @@ Domain.Other.Position = UDim2.new(0.966000021, 0, 0.261999995, 0)
 Domain.Other.Size = UDim2.new(0.0430000015, 0, 0.474999994, 0)
 
 Domain.Shadow.Name = "Shadow"
-Domain.Shadow.Parent = Domain.Main
+Domain.Shadow.Parent = Domain.Domain
 Domain.Shadow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Domain.Shadow.BackgroundTransparency = 1.000
 Domain.Shadow.BorderSizePixel = 0
@@ -67,7 +72,7 @@ Domain.NoticeMessage.Position = UDim2.new(0.0275973734, 0, 0.491011739, 0)
 Domain.NoticeMessage.Size = UDim2.new(0.272888154, 0, 0.0167460982, 0)
 Domain.NoticeMessage.ZIndex = 1000
 Domain.NoticeMessage.Font = Enum.Font.GothamBold
-Domain.NoticeMessage.Text = "Give us a second while we find the best server for you"
+Domain.NoticeMessage.Text = "{}"
 Domain.NoticeMessage.TextColor3 = Color3.fromRGB(255, 255, 255)
 Domain.NoticeMessage.TextScaled = true
 Domain.NoticeMessage.TextSize = 14.000
@@ -93,7 +98,7 @@ Domain.ShlexLogo.BorderSizePixel = 0
 Domain.ShlexLogo.Position = UDim2.new(0.939696372, 0, 0.46847716, 0)
 Domain.ShlexLogo.Size = UDim2.new(0.0411427617, 0, 0.0735122114, 0)
 Domain.ShlexLogo.ZIndex = 5
-Domain.ShlexLogo.Image = "rbxassetid://6404488837"
+Domain.ShlexLogo.Image = "rbxassetid://13030104160"
 
 Domain.SmallMessage.Name = "SmallMessage"
 Domain.SmallMessage.Parent = Domain.ServerhopAnim
@@ -104,7 +109,7 @@ Domain.SmallMessage.Position = UDim2.new(0.234, 0, 0.748, 0)
 Domain.SmallMessage.Size = UDim2.new(0.530992925, 0, 0.0167460982, 0)
 Domain.SmallMessage.ZIndex = 1000
 Domain.SmallMessage.Font = Enum.Font.GothamSemibold
-Domain.SmallMessage.Text = "imagine not using domain hub"
+Domain.SmallMessage.Text = "imagine not using Vortex"
 Domain.SmallMessage.TextColor3 = Color3.fromRGB(255, 255, 255)
 Domain.SmallMessage.TextScaled = true
 Domain.SmallMessage.TextSize = 14.000
@@ -135,22 +140,23 @@ Domain.LargeMsg.Position = UDim2.new(0.0203074999, 0, 0.0379411466, 0)
 Domain.LargeMsg.Size = UDim2.new(0.272888154, 0, 0.0347569771, 0)
 Domain.LargeMsg.ZIndex = 1000
 Domain.LargeMsg.Font = Enum.Font.GothamBold
-Domain.LargeMsg.Text = "Serverhop"
+Domain.LargeMsg.Text = ""
 Domain.LargeMsg.TextColor3 = Color3.fromRGB(255, 255, 255)
 Domain.LargeMsg.TextScaled = true
 Domain.LargeMsg.TextSize = 14.000
 Domain.LargeMsg.TextWrapped = true
 Domain.LargeMsg.TextXAlignment = Enum.TextXAlignment.Left
 
-function StartServerhop()
+function UI:UILoading(str1,str2)
+local screenUI = {}
 	Domain.ServerhopAnim.Visible = true -- checked
 	Domain.ServerhopAnim.Shadow.ImageTransparency = 1 --checked
 	Domain.LargeMsg.TextTransparency = 1
 	Domain.CancelShop.BackgroundTransparency = 1
 	Domain.CancelShop.TextTransparency = 1
 	Domain.NoticeMessage.TextTransparency = 1
-	Domain.ServerhopAnim.NoticeMessage.Text = " Give us a second while we find the best server for you"
-	Domain.LargeMsg.Text = "Serverhop"
+	Domain.ServerhopAnim.NoticeMessage.Text = str2
+	Domain.LargeMsg.Text = str1
 	Domain.ShlexLogo.ImageTransparency = 1
 	Domain.SmallMessage.TextTransparency = 1
 	Domain.NoticeMessage.Position = UDim2.new(0.028, 0, 0.491, 0)
@@ -198,10 +204,101 @@ function StartServerhop()
 	local transitionInfo = TweenInfo.new(1.25, Enum.EasingStyle.Quint)
 	local tween = game:GetService("TweenService"):Create(workspace.CurrentCamera, transitionInfo, {FieldOfView = 120})
 	tween:Play()
-	wait(0.8)
+	--wait(0.8)
 	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
 	local tween = game:GetService("TweenService"):Create(Domain.CancelShop, transitionInfo, {TextTransparency = 0})
 	tween:Play()
 	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
 	local tween = game:GetService("TweenService"):Create(Domain.ShlexLogo, transitionInfo, {ImageTransparency = 0})
+
+	function screenUI:ChangeLargeMsg(str)
+		Domain.LargeMsg.Text = str
+	end
+
+	function screenUI:ChangeNoticeMsg(str)
+		Domain.ServerhopAnim.NoticeMessage.Text = str
+	end
+	
+	return screenUI
 end
+
+local function CloseShopMenu()
+	local transitionInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.CancelShop, transitionInfo, {BackgroundTransparency = 1})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.ShlexLogo, transitionInfo, {ImageTransparency = 1})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.CancelShop, transitionInfo, {TextTransparency = 1})
+	tween:Play()
+
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.NoticeMessage, transitionInfo, {Position = UDim2.new(0.363, 0, 0.491, 0)})
+	tween:Play()
+	Domain.NoticeMessage.TextXAlignment = Enum.TextXAlignment.Center
+	shop = false
+	wait(2)
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.NoticeMessage, transitionInfo, {TextTransparency = 1})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.SmallMessage, transitionInfo, {TextTransparency = 1})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.LargeMsg, transitionInfo, {TextTransparency = 1})
+	tween:Play()
+	game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.All,true)
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.ServerhopAnim.Shadow, transitionInfo, {ImageTransparency = 1})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(1.25, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(game.Lighting:FindFirstChild("quickbro"), transitionInfo, {Size = 0})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.ToggleButton, transitionInfo, {ImageTransparency = 0})
+	tween:Play()
+	for _, audio in ipairs(workspace:GetChildren()) do
+		if audio.ClassName == "Sound" then
+			if audio:FindFirstChild("DomainHub") then
+				local transitionInfo = TweenInfo.new(0.35, Enum.EasingStyle.Quint)
+				local tween = game:GetService("TweenService"):Create(audio:FindFirstChild("DomainHub"), transitionInfo, {HighGain = -15})
+				tween:Play()
+				local transitionInfo = TweenInfo.new(0.35, Enum.EasingStyle.Quint)
+				local tween = game:GetService("TweenService"):Create(audio:FindFirstChild("DomainHub"), transitionInfo, {LowGain = -15})
+				tween:Play()
+				local transitionInfo = TweenInfo.new(0.35, Enum.EasingStyle.Quint)
+				local tween = game:GetService("TweenService"):Create(audio:FindFirstChild("DomainHub"), transitionInfo, {MidGain = 5})
+				tween:Play()
+			end
+		end
+	end
+	local transitionInfo = TweenInfo.new(0.65, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(workspace.CurrentCamera, transitionInfo, {FieldOfView = 70})
+	tween:Play()
+	wait(1.25)
+	for _, audio in ipairs(workspace:GetChildren()) do
+		if audio.ClassName == "Sound" then
+			if audio:FindFirstChild("DomainHub") then
+				audio.DomainHub:Destroy()
+			end
+		end
+	end
+	game.Lighting:FindFirstChild("quickbro"):Destroy()
+end
+
+
+Domain.CancelShop.MouseButton1Click:Connect(function()
+	Domain.ServerhopAnim.NoticeMessage.Text = " Successfully canceled"
+	Domain.LargeMsg.Text = "Stopped"
+	StopShop = true
+	local transitionInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.CancelShop, transitionInfo, {BackgroundTransparency = 1})
+	tween:Play()
+	local transitionInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quint)
+	local tween = game:GetService("TweenService"):Create(Domain.CancelShop, transitionInfo, {TextTransparency = 1})
+	tween:Play()
+	CloseShopMenu()
+end)
+
+return UI
